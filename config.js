072c5d3fcddd5b3598263154fb8e4d3218eda1bf
ElJero123 <aguirregomezjeronimo@gmail.com>
2025-07-18 18:55:59 -0500

    Archivo de configuraciones del puerto añadido

---
+++ config.js
@@ -0,0 +1,7 @@
+export const configPort = {
+  path: 'COM20', // Asegúrate de que este sea el puerto correcto
+  baudRate: 9600, // Configura según tu dispositivo
+  autoOpen: false // Desactivamos apertura automática para manejar errores
+}
+
+export const PORT = 3000
